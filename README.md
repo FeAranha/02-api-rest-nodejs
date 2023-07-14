4fbdd48dde7d5811462dba348aa1aa94ffbc8ca3
fearanha <felipearanha.c@gmail.com>
2023-07-14 18:14:59 -0300

    setup

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-#Setup
+# Setup
 - npm i
 
-#Features
+# Features
 - TS
--- src/index.js
+++
@@ -1,4 +0,0 @@
-function calcAgeUser(user) {
-    return new Date().getFullYear() - user.birthYear;
-}
-calcAgeUser({ birthYear: 1993 });
