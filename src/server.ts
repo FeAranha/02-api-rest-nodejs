cd3f19093e13bddc9e1a89f1104461c5dd1cb890
fearanha <felipearanha.c@gmail.com>
2023-07-17 13:00:22 -0300

    variáveis ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import fastify from "fastify"
-import crypto from 'node:crypto'
 import { knex } from "./database"
 
 const app = fastify()
 
 app.get('/hello', async() => {
-  const transactions = await knex('transactions').select('*') 
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*') 
   
   return transactions
 })
